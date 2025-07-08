d369cf998c1dc793da469052a529b65a6f76f8c4
AlanBWolfs <alan_blk@live.com>
2025-07-07 19:31:41 -0600

    Fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,37 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+// Se añaden los # de id's
+const $n = document.querySelector('#name');
 const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $l = document.querySelector('#location');
+const $i = document.querySelector('#avatar');
 
-function displayUser(username) {
+// Se indica función asíncrona para obtener datos de un usuario
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //Se utiliza try/catch para manejar errores
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    // Se define data en función a la función async
+    const data = await response.json();
+    localStorage.setItem('userdata', JSON.stringify(data));
+    console.log(data);
+    $n.textContent = `${data.name}`;
+    $b.innerHTML = `<a href="${data.blog}" target="_blank" rel="noopener noreferrer">${data.blog}</a>`;
+    $l.textContent = `${data.location}`;
+    // Se añade el avatar
+    $i.src = data.avatar_url;
+  } catch (err) {
+    handleError(err);
+  }
 }
+
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  // Se añade el $ faltante
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
